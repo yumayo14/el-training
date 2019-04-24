db6ae5024b977eff605f6bf8cfea3961675c3dbf
y.yoshioka <y.yoshioka@ashita-team.com>
2019-04-24 18:07:52 +0900

    refactor: spec/support/capybara.rbで呼び出す必要がないのにクラスの呼び出しだけしていた(Warning)

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,7 +5,6 @@ require 'selenium-webdriver'
 
 Capybara.javascript_driver = :selenium_chrome
 Capybara.default_max_wait_time = 3
-Capybara
 
 Capybara.register_driver :selenium_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
