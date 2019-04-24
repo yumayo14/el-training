105cf18309274403bd20149874ce7d823ebd75ea
y.yoshioka <y.yoshioka@ashita-team.com>
2019-04-24 17:35:16 +0900

    refactor: ブロック処理を{}で行う際は前後にスペースをつける

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ApplicationRecord
   enum importance: { 低: 0, 中: 1, 高: 2 }
   enum status: { 未着手: 0, 着手: 1, 完了: 2 }
 
-  validates :title, presence: true, length: {maximum: 30}
+  validates :title, presence: true, length: { maximum: 30 }
   validates :importance, inclusion: { in: %w(低 中 高) }
   validates :status, inclusion: { in: %w(未着手 着手 完了) }
   validate :dead_line_on_cannot_be_in_the_past
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -24,14 +24,14 @@ RSpec.describe Task, type: :model do
     end
     context 'importanceが(低, 中, 高)以外の場合' do
       it '作成できない' do
-        expect{FactoryBot.build(:task, importance: 'sasa')}.to raise_error(ArgumentError)
-        expect{ FactoryBot.build(:task, importance: 99)}.to raise_error(ArgumentError)
+        expect { FactoryBot.build(:task, importance: 'sasa') }.to raise_error(ArgumentError)
+        expect { FactoryBot.build(:task, importance: 99) }.to raise_error(ArgumentError)
       end
     end
     context 'statusが(未着手, 着手, 完了)以外の場合' do
       it '作成できない' do
-        expect{FactoryBot.build(:task, status: 'sasa')}.to raise_error(ArgumentError)
-        expect{FactoryBot.build(:task, status: 99)}.to raise_error(ArgumentError)
+        expect { FactoryBot.build(:task, status: 'sasa') }.to raise_error(ArgumentError)
+        expect { FactoryBot.build(:task, status: 99) }.to raise_error(ArgumentError)
       end
     end
     context '期限が過去の日付の場合' do
