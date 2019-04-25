c8a5f571fd7005308f321854a14017b0b0632b62
y.yoshioka <y.yoshioka@ashita-team.com>
2019-04-25 16:32:49 +0900

    refactor: name空間でapi::となっている箇所が子クラスに間違われないように修正

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Api::TasksController < ApplicationController
+class Api::TasksController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
   def index
     @tasks = Task.order(created_at: 'DESC').page(params[:page]).per(10)
   end
--- app/helpers/api/tasks_helper.rb
+++ app/helpers/api/tasks_helper.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-module Api::TasksHelper
+module Api::TasksHelper # rubocop:disable Style/ClassAndModuleChildren
 end
