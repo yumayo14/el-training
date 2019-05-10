dcaa3ca9181255d81b080f7125275835b1ff571a
y.yoshioka <y.yoshioka@ashita-team.com>
2019-05-10 12:37:54 +0900

    refactor: enumでバインドしている文字が日本語だったので、英語に変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  enum importance: { 低: 0, 中: 1, 高: 2 }
-  enum status: { 未着手: 0, 着手: 1, 完了: 2 }
+  enum importance: { low: 0, middle: 1, high: 2 }
+  enum status: { not_started: 0, working: 1, completed: 2 }
 
   validates :title, presence: true, length: { maximum: 30 }
-  validates :importance, inclusion: { in: %w(低 中 高) }
-  validates :status, inclusion: { in: %w(未着手 着手 完了) }
+  validates :importance, inclusion: { in: %w(low middle high) }
+  validates :status, inclusion: { in: %w(not_started working completed) }
   validate :dead_line_on_cannot_be_in_the_past
 
   scope :orderd_by, ->(sort) { order(created_at: sort) }
