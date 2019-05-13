b6be37a6144fd931fefa8985abcc551108c256a5
y.yoshioka <y.yoshioka@ashita-team.com>
2019-05-13 17:29:38 +0900

    feat: Userのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :accountid, presence: true, length: { maximum: 15 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
 end
