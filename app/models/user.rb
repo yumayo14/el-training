b94d1963b90e302282f4fcd345045c22efc5cc3d
y.yoshioka <y.yoshioka@ashita-team.com>
2019-05-13 17:53:39 +0900

    refactor: rubocopに指摘された箇所を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :accountid, presence: true, length: { maximum: 15 }, uniqueness: true
