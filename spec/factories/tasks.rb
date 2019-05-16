d91799d56043ed02b84a4e72e45922f5ccfddd04
y.yoshioka <y.yoshioka@ashita-team.com>
2019-05-16 16:41:04 +0900

    docs: スキーマファイルの情報をコメントとして書き出す

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id           :bigint           not null, primary key
+#  user_id      :bigint
+#  title        :string(255)      not null
+#  importance   :integer          default("low")
+#  dead_line_on :date
+#  status       :integer          default("not_started")
+#  detail       :text(65535)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 FactoryBot.define do
   factory :task do
     sequence(:title) { |n| "#{n}番目" }
