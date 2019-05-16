d00d22c2af694d3353d9b0d9ab9a9f770b7b6ddd
y.yoshioka <y.yoshioka@ashita-team.com>
2019-05-16 11:02:28 +0900

    chore: gem bulletの設定を追記

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,6 +3,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # 開発環境での、gem bulletの設定
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.raise = true
+    Bullet.bullet_logger = true
+    Bullet.rails_logger  = true
+  end
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
