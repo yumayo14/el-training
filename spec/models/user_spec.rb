47398d9225f085f1a585493843ecfe07705a4949
Yuma Yoshioka <31206922+yumayo14@users.noreply.github.com>
2019-05-29 15:42:51 +0900

    ユーザーの更新時、勝手にhashed_passwordの値が書き換わるバグの修正 (#99)
    * feat: passwordという仮想属性を追加
    
    * fix: 更新時、パスワード情報が更新されなければ、hashed_passwordを変えないようにする
    
    * test: 既存のテストコードを修正
    
    * test: 更新時のパスワードの暗号化をテスト
    
    * reviewd: パスワードが更新された場合、passwordのバリデーションが実行されるように指定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,11 +16,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'バリデーション' do
-    let(:user) { build(:user, name: name, accountid: accountid, hashed_password: hashed_password) }
+    let(:user) { build(:user, name: name, accountid: accountid, password: password) }
     let(:name) { '田中 太郎' }
     let(:accountid) { 'tanatarou' }
-    let(:hashed_password) { 'morethan8' }
-    subject { user.valid? }
+    let(:password) { 'morethan8' }
+    subject { user.valid?(:password_change) }
     context '名前が20文字以内の場合' do
       it { is_expected.to eq true }
     end
@@ -44,7 +44,7 @@ RSpec.describe User, type: :model do
       it { is_expected.to eq false }
     end
     context '既存のユーザーと同じアカウントIDで登録しようとした場合' do
-      let!(:same_accountid_user) { create(:user, name: '田中 宏和', accountid: 'tanahiro', hashed_password: 'morethan8') }
+      let!(:same_accountid_user) { create(:user, name: '田中 宏和', accountid: 'tanahiro', password: 'morethan8') }
       let(:accountid) { 'tanahiro' }
       it { is_expected.to eq false }
     end
@@ -52,25 +52,49 @@ RSpec.describe User, type: :model do
       it { is_expected.to eq true }
     end
     context 'パスワードが入力されていない場合' do
-      let(:hashed_password) { nil }
+      let(:password) { nil }
       it { is_expected.to eq false }
     end
     context 'パスワードが8文字より少ない場合' do
-      let(:hashed_password) { '1a2b3c4' }
+      let(:password) { '1a2b3c4' }
       it { is_expected.to eq false }
     end
   end
   describe 'パスワードの暗号化' do
-    let!(:user) { create(:user, hashed_password: hashed_password) }
-    let(:hashed_password) { 'morethan8' }
+    let!(:user) { create(:user, password: password) }
+    let(:password) { 'morethan8' }
     describe '平文を含まない様に暗号化している' do
       subject { user.hashed_password }
-      it { is_expected.not_to include hashed_password }
+      it { is_expected.not_to include password }
+    end
+    context 'ユーザー情報が更新された場合' do
+      let!(:previous_hashed_password) { user.hashed_password }
+      context 'パスワードが更新された場合' do
+        before { user.password = new_password }
+        context '新しいパスワードが8文字より多い場合' do
+          let(:new_password) { 'newpassword' }
+          subject { user.save(context: :password_change) }
+          it { is_expected.to eq true }
+        end
+        context '新しいパスワードが8文字より少ない場合' do
+          let(:new_password) { 'new' }
+          subject { user.save(context: :password_change) }
+          it { is_expected.to eq false }
+        end
+      end
+      context 'パスワード以外の情報が更新された場合' do
+        let(:new_name) { 'newname' }
+        before { user.update(name: new_name) }
+        describe 'パスワード情報は更新されない' do
+          subject { user.hashed_password }
+          it { is_expected.to eq previous_hashed_password }
+        end
+      end
     end
   end
   describe 'メソッド' do
-    let!(:user) { create(:user, hashed_password: hashed_password) }
-    let(:hashed_password) { 'morethan8' }
+    let!(:user) { create(:user, password: password) }
+    let(:password) { 'morethan8' }
     describe '#authenticated?' do
       let(:login_password) { '' }
       subject { user.authenticated?(login_password) }
