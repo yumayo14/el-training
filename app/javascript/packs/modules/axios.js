7f0414c0786a649992d84f3c622f63c516ee243e
y.yoshioka <y.yoshioka@ashita-team.com>
2019-06-05 16:50:38 +0900

    refactor: モジュールを作成する場青、それを説明しるJSDocをかく

--- app/javascript/packs/modules/axios.js
+++ app/javascript/packs/modules/axios.js
@@ -1,10 +1,14 @@
 import axios from 'axios';
 
+/**
+ * 各リクエストの際、必要な設定をしたaxiosを返す関数。
+ * @return {axios} CSRFトークンをリクエストヘッダに含めたaxiosを返す
+ */
 function prepareAxios() {
   return axios.create({
     headers: {
       'X-Requested-With': 'XMLHttpRequest',
-      'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+      'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
     },
   });
 }
