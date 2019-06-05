04541b898c1c07c61994a58312efa2bd5fa31508
y.yoshioka <y.yoshioka@ashita-team.com>
2019-06-05 15:25:13 +0900

    refactor: indentを4spacesから2spacesに変更

--- app/javascript/packs/modules/axios.js
+++ app/javascript/packs/modules/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 function prepareAxios() {
-    return axios.create({
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-        }
-    });
+  return axios.create({
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    }
+  });
 }
 
 export default prepareAxios;
