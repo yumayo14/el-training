505b76a1198acfa0f69c4d82116ca7dc6279dac5
y.yoshioka <y.yoshioka@ashita-team.com>
2019-06-10 16:51:28 +0900

    refactor: ESLintで指摘された箇所を修正

--- app/javascript/packs/sessions/logout_vue.js
+++ app/javascript/packs/sessions/logout_vue.js
@@ -1,18 +1,17 @@
 import Vue from 'vue';
 import prepareAxios from '../modules/axios';
-import toastr from 'toastr';
 
-window.logoutLink = new Vue ({
+window.logoutLink = new Vue({
   el: '#logout_link',
   data: {
-    logout_url: '/logout'
+    logout_url: '/logout',
   },
   methods: {
     requestLogout: function() {
       prepareAxios({withCsrf: true, withCookie: true}).delete(this.logout_url)
-      .then(function(response) {
-        window.location.href = response.data;
-      });
+        .then(function(response) {
+          window.location.href = response.data;
+        });
     },
-  }
+  },
 });
