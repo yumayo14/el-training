e55e9ff13d128e11083b501bfa1457c0e29808e0
y.yoshioka <y.yoshioka@ashita-team.com>
2019-06-19 16:19:00 +0900

    test: 一覧表示のテストを絞り込みを行わない場合というcontextで行うようにする

--- spec/requests/api/tasks_spec.rb
+++ spec/requests/api/tasks_spec.rb
@@ -9,17 +9,21 @@ RSpec.describe 'Api::Tasks', type: :request do
       accountid: 'tester',
       password: 'IamTestMan'
     }
-    2.times { create(:task, title: 'login_userd_task', user: user) }
-    3.times { create(:task) }
   end
   describe 'GET#index' do
     context 'ログインを行なっている場合' do
-      it '自分が投稿したタスクのみが表示される' do
-        get api_tasks_path
-        login_user_tasks = JSON.parse(response.body)['nested']['data']
-        expect(login_user_tasks.length).to eq 2
-        expect(login_user_tasks[0]['title']).to eq 'login_userd_task'
-        expect(login_user_tasks[1]['title']).to eq 'login_userd_task'
+      context '絞り込み検索を行わない場合' do
+        before do
+          2.times { create(:task, title: 'login_userd_task', user: user) }
+          3.times { create(:task) }
+        end
+        it '自分が投稿したタスクのみが表示される' do
+          get api_tasks_path
+          login_user_tasks = JSON.parse(response.body)['nested']['data']
+          expect(login_user_tasks.length).to eq 2
+          expect(login_user_tasks[0]['title']).to eq 'login_userd_task'
+          expect(login_user_tasks[1]['title']).to eq 'login_userd_task'
+        end
       end
     end
   end
