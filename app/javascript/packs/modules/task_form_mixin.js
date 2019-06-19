f3a7c70c4c16e3d9cfcbdf2bde895aa34e85e8c9
y.yoshioka <y.yoshioka@ashita-team.com>
2019-06-19 18:29:24 +0900

    refactor: タスク作成、更新の両方に共通する部分をmixinできるようにする

--- app/javascript/packs/modules/task_form_mixin.js
+++ app/javascript/packs/modules/task_form_mixin.js
@@ -1,7 +1,10 @@
-const taskFormDataSharable = {
+import prepareAxios from './axios';
+import toastr from 'toastr';
+
+export default {
   data: function() {
     return {
-      create_url: '/api/tasks',
+      url: '',
       title: '',
       importance: 'low',
       dead_line_on: '',
@@ -10,6 +13,31 @@ const taskFormDataSharable = {
       processing_request: false,
     };
   },
+  methods: {
+    requestUrl: function() {
+      prepareAxios({withCsrf: true, withCookie: false}).post(this.url,
+                                                             new URLSearchParams({
+                                                               'title': this.title,
+                                                               'importance': this.importance,
+                                                               'dead_line_on': this.dead_line_on,
+                                                               'status': this.status,
+                                                               'detail': this.detail})
+      ).then((response)=> {
+        window.location.href = response.data.redirect_url;
+      }).catch((error)=> {
+        error.response.data.forEach(function(message) {
+          toastr.error(message);
+        });
+      }).finally(()=> {
+        this.reloadForm(1000);
+      });
+    },
+    reloadForm: function(time) {
+      (new Promise((resolve)=> {
+        resolve(this.processing_request = true);
+      })).then(function() {
+        setTimeout(()=> this.processing_request = false, time);
+      }.bind(this));
+    },
+  },
 };
-
-export default taskFormDataSharable;
--- app/javascript/packs/tasks/task_form_vue.js
+++ app/javascript/packs/tasks/task_form_vue.js
@@ -1,29 +1,10 @@
 import Vue from 'vue/dist/vue.esm.js';
-import formReloadable from '../modules/form_common_mixin';
-import taskFormSharable from '../modules/task_form_mixin';
-import prepareAxios from '../modules/axios';
-import toastr from 'toastr';
+import taskFormRequestable from '../modules/task_form_mixin';
 
 window.task_form = new Vue({
   el: '#task_form',
-  mixins: [formReloadable, taskFormSharable],
-  methods: {
-    createTask: function() {
-      prepareAxios({withCsrf: true, withCookie: false}).post(this.create_url,
-                                                             new URLSearchParams({'title': this.title,
-                                                                                  'importance': this.importance,
-                                                                                  'dead_line_on': this.dead_line_on,
-                                                                                  'status': this.status,
-                                                                                  'detail': this.detail})
-      ).then((response)=> {
-        window.location.href = response.data.redirect_url;
-      }).catch((error)=> {
-        error.response.data.forEach(function(message) {
-          toastr.error(message);
-        });
-      }).finally(()=> {
-        this.reloadForm(1000);
-      });
-    },
+  mixins: [taskFormRequestable],
+  data: {
+    url: '/api/tasks',
   },
 });
