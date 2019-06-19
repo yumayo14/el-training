6eac80c6fa5a9bdbf491816e6d4d38e3230d58c1
y.yoshioka <y.yoshioka@ashita-team.com>
2019-06-19 16:22:15 +0900

    test: 絞り込み検索のテストをrequest_specで行うようにする

--- spec/requests/api/tasks_spec.rb
+++ spec/requests/api/tasks_spec.rb
@@ -25,6 +25,50 @@ RSpec.describe 'Api::Tasks', type: :request do
           expect(login_user_tasks[1]['title']).to eq 'login_userd_task'
         end
       end
+      context '絞り込み検索を行う場合' do
+        let!(:task_for_title_search) { create(:task, title: 'search_test', status: 'completed', user: user) }
+        let!(:task_for_status_search) { create(:task, title: 'not_matching', status: 'working', user: user) }
+        let!(:same_title_status_task) { create(:task, title: 'search', status: 'working', user: user) }
+        let(:title) { '' }
+        let(:status) { '' }
+        let(:search_request) do
+          get api_tasks_path, params: {
+            title: title,
+            status: status
+          }
+        end
+        context 'タスク名でのみ検索する場合' do
+          let(:title) { 'search' }
+          it '検索文言とタイトル名が部分一致するタスクのみ表示される' do
+            search_request
+            searched_tasks = JSON.parse(response.body)['nested']['data']
+            expect(searched_tasks.length).to eq 2
+            expect(searched_tasks[0]['title']).to include title
+            expect(searched_tasks[1]['title']).to include title
+          end
+        end
+        context 'ステータスのみで検索する場合' do
+          let(:status) { 'working' }
+          it '同じステータスのタスクのみが表示される' do
+            search_request
+            searched_tasks = JSON.parse(response.body)['nested']['data']
+            expect(searched_tasks.length).to eq 2
+            expect(searched_tasks[0]['status']['text']).to eq '着手'
+            expect(searched_tasks[1]['status']['text']).to eq '着手'
+          end
+        end
+        context 'タスク名とステータスで検索する場合' do
+          let(:title) { 'search' }
+          let(:status) { 'working' }
+          it '検索文言とタイトル名が部分一致した上でステータスが同じタスクのみ表示される' do
+            search_request
+            searched_tasks = JSON.parse(response.body)['nested']['data']
+            expect(searched_tasks.length).to eq 1
+            expect(searched_tasks[0]['title']).to include title
+            expect(searched_tasks[0]['status']['text']).to eq '着手'
+          end
+        end
+      end
     end
   end
 end
