b14353daebc7bc4d116c561018798fe163fc9f83
y.yoshioka <y.yoshioka@ashita-team.com>
2019-06-28 18:41:44 +0900

    feat: MdTableコンポーネントが参照しているコンポーネントもインポートするようにする

--- app/javascript/packs/tasks/index_vue.js
+++ app/javascript/packs/tasks/index_vue.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import requestByConfiguredAxios from '../modules/request_by_configured_axios';
 import toastr from 'toastr';
 import updateTaskForm from '../modules/update_task_form_component.js';
-import {MdTable} from 'vue-material/dist/components';
+import {MdContent, MdRipple, MdTable} from 'vue-material/dist/components';
 import VuePaginator from 'vuejs-paginator';
 import _ from 'lodash';
 import 'toastr/toastr.scss';
@@ -11,6 +11,8 @@ import '../../stylesheets/application.scss';
 
 Vue.prototype.$http = axios;
 
+Vue.use(MdRipple);
+Vue.use(MdContent);
 Vue.use(MdTable);
 
 window.tasks = new Vue({
