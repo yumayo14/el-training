7acafd7016e8d6d1631b5f205da93bb1e7e3b797
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-01 18:01:25 +0900

    refactor: CIが通るように修正

--- lib/apis/open_weather_map.rb
+++ lib/apis/open_weather_map.rb
@@ -1,11 +1,13 @@
-API_KEY = ENV["EL_TRAINING_WEATER_API_KEY"]
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+# frozen_string_literal: true
+
+API_KEY = ENV['EL_TRAINING_WEATER_API_KEY']
+BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 class OpenWeatherMap
   def fetch_weather_info
     uri = URI.parse(BASE_URL + "?q=Tokyo,jp&APPID=#{API_KEY}")
     response = Net::HTTP.get_response(uri)
-    
+
     JSON.parse(response.body)
   end
 end
