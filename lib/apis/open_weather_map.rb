3ff35d13a9c49c08bd8e45c3806261be65b3ddca
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-01 17:53:32 +0900

    refactor: ファイル名とクラスめいを変更し、使用していない一時変数を削除

--- lib/apis/open_weather_map.rb
+++ lib/apis/open_weather_map.rb
@@ -1,10 +1,11 @@
 API_KEY = ENV["EL_TRAINING_WEATER_API_KEY"]
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
-class OpenWeather
+class OpenWeatherMap
   def fetch_weather_info
     uri = URI.parse(BASE_URL + "?q=Tokyo,jp&APPID=#{API_KEY}")
     response = Net::HTTP.get_response(uri)
-    data = JSON.parse(response.body)
+    
+    JSON.parse(response.body)
   end
 end
