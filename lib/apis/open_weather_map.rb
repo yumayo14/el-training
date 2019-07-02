b75a578613436c729dbe29ddb46a5cf27844dc56
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-02 09:57:49 +0900

    feat: open-uriを用いてAPIを叩いて、気温データをセルシウス表記で取得できるようにする

--- lib/apis/open_weather_map.rb
+++ lib/apis/open_weather_map.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-API_KEY = ENV['EL_TRAINING_WEATER_API_KEY']
+require 'open-uri'
+
+API_KEY = ENV['EL_TRAINING_OPEN_WEATHER_MAP_API_KEY']
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 class OpenWeatherMap
-  def fetch_weather_info
-    uri = URI.parse(BASE_URL + "?q=Tokyo,jp&APPID=#{API_KEY}")
-    response = Net::HTTP.get_response(uri)
-
-    JSON.parse(response.body)
+  def self.fetch_current_tokyo_weather
+    JSON.parse(OpenURI.open_uri(BASE_URL + "?q=Tokyo,jp&units=metric&APPID=#{API_KEY}").read)
   end
 end
