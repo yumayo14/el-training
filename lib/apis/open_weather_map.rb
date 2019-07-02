fe510c9885f4da08ba8a818311e822ea84fe090d
Yuma Yoshioka <31206922+yumayo14@users.noreply.github.com>
2019-07-02 16:49:08 +0900

    OpenWeatherMap APIを叩いて東京の気象情報を取得できるようにする (#138)
    * feat: OpenWeatherMap Apiを叩いて東京の気象情報を取得する
    
    * feat: lib/apis配下のファイルをがrailsに読み込まれるように変更
    
    * refactor: ファイル名とクラスめいを変更し、使用していない一時変数を削除
    
    * refactor: CIが通るように修正
    
    * feat: open-uriを用いてAPIを叩いて、気温データをセルシウス表記で取得できるようにする
    
    * chore: 外部APIキーをcredentials.yml.encで管理するようにする
    
    * fix: 定数の宣言をクラス内で行うようにする
    
    * refactor: 文字列の連結は式展開で行うようにする

---
+++ lib/apis/open_weather_map.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+
+class OpenWeatherMap
+  API_KEY = Rails.application.credentials.api[:open_weather_map_keys]
+  BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+  def self.fetch_current_tokyo_weather
+    JSON.parse(OpenURI.open_uri("#{BASE_URL}?q=Tokyo,jp&units=metric&APPID=#{API_KEY}").read)
+  end
+end
