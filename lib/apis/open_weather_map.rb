4715d8bc7c2c9497e212ca3d7ba2ad26d2174cdf
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-02 12:21:09 +0900

    fix: 定数の宣言をクラス内で行うようにする

--- lib/apis/open_weather_map.rb
+++ lib/apis/open_weather_map.rb
@@ -2,10 +2,10 @@
 
 require 'open-uri'
 
-API_KEY = Rails.application.credentials.api[:open_weather_map_keys]
-BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
-
 class OpenWeatherMap
+  API_KEY = Rails.application.credentials.api[:open_weather_map_keys]
+  BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
   def self.fetch_current_tokyo_weather
     JSON.parse(OpenURI.open_uri(BASE_URL + "?q=Tokyo,jp&units=metric&APPID=#{API_KEY}").read)
   end
