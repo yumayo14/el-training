4368bcf0f76d77307df1b9b3c6a2515aeb670b8b
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-03 15:27:29 +0900

    refactor: 必要がないbefore_actionを使わないようにする

--- app/controllers/api/open_weather_maps_controller.rb
+++ app/controllers/api/open_weather_maps_controller.rb
@@ -1,19 +1,12 @@
 # frozen_string_literal: true
 
 class Api::OpenWeatherMapsController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
-  before_action :fetch_current_tokyo_weather
-
   def current_tokyo_weather
+    @weather = OpenWeatherMap.fetch_current_tokyo_weather
     if @weather['cod'] != 200
       render json: '天気情報の取得に失敗しました', status: @weather['cod']
     else
       render json: @weather, status: 200
     end
-  end
-
-  private
-
-  def fetch_current_tokyo_weather
-    @weather = OpenWeatherMap.fetch_current_tokyo_weather
   end
 end
