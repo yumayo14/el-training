f0fd8ace4a4c32e6cdebd926620128274d5bf544
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-03 15:30:09 +0900

    refactor: taskのapiと同じように成功時の処理を先に書くようにする

--- app/controllers/api/open_weather_maps_controller.rb
+++ app/controllers/api/open_weather_maps_controller.rb
@@ -3,10 +3,10 @@
 class Api::OpenWeatherMapsController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
   def current_tokyo_weather
     @weather = OpenWeatherMap.fetch_current_tokyo_weather
-    if @weather['cod'] != 200
-      render json: '天気情報の取得に失敗しました', status: @weather['cod']
-    else
+    if @weather['cod'] == 200
       render json: @weather, status: 200
+    else
+      render json: '天気情報の取得に失敗しました', status: @weather['cod']
     end
   end
 end
