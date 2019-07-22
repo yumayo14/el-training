de92ec5e9081d3e83ab7ff8e4adecb9ccd36e3a5
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-22 18:57:53 +0900

    style: 一覧表示部分の投稿フォーム、絞り込みフォーム部分、状態と期限を表示するボタンを作成

--- app/frontend/packs/javascript/issues_vue.js
+++ app/frontend/packs/javascript/issues_vue.js
@@ -1,13 +1,24 @@
 import Vue from 'vue/dist/vue.esm.js';
-import {MdContent, MdRipple, MdTable, MdCard, MdLayout} from 'vue-material/dist/components';
+import {MdContent, MdRipple, MdTable, MdCard, MdLayout, MdButton} from 'vue-material/dist/components';
 import '../stylesheets/application.scss';
+import newIssueForm from "./modules/update_task_form_component";
 
 Vue.use(MdRipple);
 Vue.use(MdContent);
 Vue.use(MdTable);
 Vue.use(MdCard);
 Vue.use(MdLayout);
+Vue.use(MdButton);
 
 window.issues = new Vue({
-  el: '#issues'
+  el: '#issues',
+  data: {
+    method: 'get',
+    request_url: '/api/issues/',
+    issues: [],
+    selectedStatus: '',
+  },
+  components: {
+    newIssueForm: newIssueForm,
+  }
 });
