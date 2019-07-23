42daad372572fe9f31caad793e0b1a93624f5a5f
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-23 10:21:42 +0900

    feat: 作成したissuesのindexページに遷移できるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
 
   root to: 'tasks#index'
   resources :tasks, except: %i(create destroy)
+  resources :issues, only: :index
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
