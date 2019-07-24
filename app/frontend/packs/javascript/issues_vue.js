d315707724c30b8250d7b233ea797dcc498a25fb
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-24 10:10:57 +0900

    refactor: ESLintに指摘されたが修正できていなかった箇所を修正

--- app/frontend/packs/javascript/issues_vue.js
+++ app/frontend/packs/javascript/issues_vue.js
@@ -2,7 +2,6 @@ import Vue from 'vue/dist/vue.esm.js';
 import {MdContent, MdRipple, MdTable, MdCard, MdLayout, MdButton} from 'vue-material/dist/components';
 import '../stylesheets/application.scss';
 import newIssueForm from './modules/new_issue_form_component';
-import stepCircleComponent from './modules/step_circle_component';
 
 Vue.use(MdRipple);
 Vue.use(MdContent);
