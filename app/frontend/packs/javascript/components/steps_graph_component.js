e8526b22efc72493504e6b32add897076e8e1a5b
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-29 16:13:14 +0900

    refactor: ESLintに指摘された箇所を修正

--- app/frontend/packs/javascript/components/steps_graph_component.js
+++ app/frontend/packs/javascript/components/steps_graph_component.js
@@ -1,4 +1,4 @@
-import { Doughnut } from 'vue-chartjs';
+import {Doughnut} from 'vue-chartjs';
 
 export default {
   extends: Doughnut,
@@ -8,23 +8,23 @@ export default {
         labels: ['完了', '未完了'],
         datasets: [{
           data: [70, 30],
-          backgroundColor: ['#4CAF50', '#EF8157']
-        }]
+          backgroundColor: ['#4CAF50', '#EF8157'],
+        }],
       },
       option: {
         cutoutPercentage: 75,
         legend: {
           labels: {
             // このフォントプロパティ指定は、グローバルプロパティを上書きします
-            fontColor: 'white'
-          }
-        }
-      }
-    }
+            fontColor: 'white',
+          },
+        },
+      },
+    };
   },
   methods: {
   },
   mounted: function() {
     this.renderChart(this.graphData, this.option);
-  }
+  },
 };
