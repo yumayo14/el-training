a2ff2b7e7b4d04bedb9297e702127acdf56d461f
y.yoshioka <y.yoshioka@ashita-team.com>
2019-07-30 17:09:37 +0900

    feat: 一覧表示画面から詳細画面へ遷移できるようにする

--- app/frontend/packs/javascript/components/each_issue_component.js
+++ app/frontend/packs/javascript/components/each_issue_component.js
@@ -2,11 +2,13 @@ export default {
   props: ['issue'],
   template: `
             <md-card class="each_issue">
-              <md-card-content>
-                <h4 class="issue_title">
-                  {{issue.title}}
-                </h4>
-              </md-card-content>
+              <a class="issue_link" href="/issues/{{issue.id}}">
+                <md-card-content>
+                  <h4 class="issue_title">
+                    {{issue.title}}
+                  </h4>
+                </md-card-content>
+              </a>
               <md-card-actions class="md-alignment-left">
                 <div class="issue_status">
                   <md-button class="md-primary">
