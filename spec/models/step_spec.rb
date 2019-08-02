aa59ca8c77c12bd087c73776f9d9e15a7a6e73d0
y.yoshioka <y.yoshioka@ashita-team.com>
2019-08-02 14:32:03 +0900

    test: Stepのmodel_specでバリデーションのテストを作成

--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -6,10 +6,30 @@
 #  title      :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  issue_id   :bigint
+#
+# Indexes
+#
+#  index_steps_on_issue_id  (issue_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (issue_id => issues.id)
 #
 
 require 'rails_helper'
 
 RSpec.describe Step, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Stepのバリデーション' do
+    let(:step) { build(:step, title: title) }
+    subject { step.valid? }
+    context 'タイトルが入力されている場合' do
+      let(:title) { 'ステップ' }
+      it { is_expected. to eq true }
+    end
+    context 'タイトルが入力されていない場合' do
+      let(:title) { '' }
+      it { is_expected. to eq false }
+    end
+  end
 end
